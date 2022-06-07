97bba7284ff00314a3dd50e30d891fc75ee217c3
Stephen Osserman <stephen.osserman@gmail.com>
2022-06-06 23:33:30 -0700

    remaining initial files

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,18 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
+
+const dev = (process.env.NODE_ENV == 'development') 
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+            pages: "docs",
+            assets: "docs"
+        }),
+        paths: {
+            // change below to your repo name
+            base: dev ? "" : "/sveltekit_test",
+        }
 	}
 };
 
